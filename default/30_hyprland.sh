5b8c328047ee83a6700147b8b0598b29fbafc25b
Luis Schmidmeister <vzbls@t-online.de>
2024-09-26 22:25:59 +0200

    testing

--- default/30_hyprland.sh
+++ default/30_hyprland.sh
@@ -2,3 +2,7 @@
 
 sudo pacman -S --noconfirm kitty
 sudo pacman -S --noconfirm hyprland
+
+sudo pacman -S --noconfirm wofi
+sudo pacman -S --noconfirm waybar
+sudo pacman -S --noconfirm ttf-font-awesome
